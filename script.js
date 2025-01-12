a7ab6da604eac66f7d91ae4b2740e0e9c0768cec
Trace-Gary <tracegary@gmail.com>
2025-01-12 16:01:15 -0600

    Updated. Trying to get the slider to cooperate

---
+++ script.js
@@ -0,0 +1,13 @@
+// script.js
+document.getElementById('shop').addEventListener('click', function () {
+    // Navigate to shop.html
+    window.location.href = 'shop.html';
+});
+  
+
+document.getElementById('index').addEventListener('click', function () {
+    // Navigate to index.html
+    window.location.href = 'index.html';
+});
+
+
